5e8c8de1e79ae4354417163a888a6906e75b743d
hatout01 <hatout1@gmail.com>
2020-02-24 18:37:07 -0800

    added users schema

--- models/users.js
+++ models/users.js
@@ -1,1 +1,48 @@
+const mongoose = require("mongoose");
 
+const Schema = mongoose.Schema;
+
+let UserSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String
+    //    required: true
+  },
+  userName: {
+    type: String
+    // required: true
+  },
+  email: {
+    type: String
+    // required: true
+  },
+  passWord: {
+    type: String
+    // required: true
+  },
+  image: {
+    type: String
+  },
+  articlePosted: {
+    type: String
+  },
+  post: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Post"
+    }
+  ],
+  comments: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Comment"
+    }
+  ]
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
