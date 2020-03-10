817964b4f37e208d9f0eef0bbced62b5e47d523e
Larry-nan <lawrence,yinan@gmail.com>
2020-03-09 18:32:47 -0700

    created signup and login pages

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -1,11 +1,17 @@
-
 import React, { Component } from 'react'
+import "./Footer.css"
 
  class Footer extends Component {
     render() {
         return (
             <div>
-               <footer>Copy right</footer> 
+               <footer>Copy right
+               Larry Nan 
+               Huan Ngyuen 
+               Herman Liu 
+               Mohamed Htout 
+               Sarah Naas
+                   </footer> 
             </div>
         )
     }
---
+++ client/src/pages/signUp/signUp.js
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+import "./signUp.css";
+import API from "../../Components/utility/API";
+
+class SignUp extends Component {
+  state = {
+    user: "",
+    password: "",
+    babyname: "",
+    babyimg: ""
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+
+    API.register({
+      user: this.state.user,
+      password: this.state.password,
+      babyname: this.state.babyname,
+      babyimg: this.state.babyimg
+    }).then(res =>
+      this.setState({
+        user: "",
+        password: "",
+        babyname: "",
+        babyimg: ""
+      })
+    );
+    window.location.assign("/");
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <h2>SignUp</h2>
+        <form>
+          <div className="form-group">
+            <label>Email or User Name</label>
+            <input
+              type="string"
+              className="form-control"
+              id="newEmail"
+              placeholder="Enter email or user name"
+              name="user"
+              onChange={this.handleInputChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="newPassword"
+              placeholder="*******"
+              name="password"
+              onChange={this.handleInputChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Confirm Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="confirmPassword"
+              placeholder="*******"
+              name="password"
+              onChange={this.handleInputChange}
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="btn btn-info"
+            onClick={this.handleFormSubmit}
+          >
+            SignUp
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignUp;
