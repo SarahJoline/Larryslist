7a86fccdf42face8a21620fdbebaf625e1098b45
Larry-nan <lawrence,yinan@gmail.com>
2020-03-20 15:50:12 -0700

    deploy

--- client/src/Components/Display/Display.js
+++ client/src/Components/Display/Display.js
@@ -3,9 +3,9 @@ import React from "react";
 function Display(props) {
   return (
     <div>
-      {props.items.map(item => {
+      {props.items.map((item,index) => {
         return (
-          <React.Fragment>
+          <React.Fragment key={index}>
             <div>{item.title}</div>
             <div>{item.description}</div>
           </React.Fragment>
