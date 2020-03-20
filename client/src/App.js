fc10b39196caf16b4702c77232f66556f2dfaa03
Larry-nan <lawrence,yinan@gmail.com>
2020-03-20 16:28:04 -0700

    heroku fix

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ import Favorite from "./pages/Favorite/Favorite";
 import Home from "./pages/Home/Home";
 import Footer from "./Components/Footer/Footer";
 import "./App.css";
-import Login from "./Components/Login/Login.js";
+import Login from "./Components/Login/login.js";
 import Header from "./Components/Header/Header";
 import Navbar from "./Components/Navbar/Navbar";
 import NewPost from "./Components/NewPost/NewPost";
