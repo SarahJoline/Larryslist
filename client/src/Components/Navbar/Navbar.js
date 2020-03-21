b61621b6e1fd754a99d31c10e84f4b8ed3a9f119
Sarah Naas <sjnaas@gmail.com>
2020-03-21 01:22:07 -0700

    styling nav bar

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-
 import "./Navbar.css";
 import { navigate } from "@reach/router";
-
 function Navbar(props) {
   let path = useLocation().pathname;
   console.log("path", path);
@@ -35,63 +33,115 @@ function Navbar(props) {
   //       </button>
   //     )}
   // )}
-console.log("navbar",props.user);
-
+  console.log("navbar", props.user);
   return (
     <div className="Wrapper">
-      <div className="navbar navbar-expand-lg navbar-light bg-light">
-        <button>
-          <Link to="/home">Home</Link>
-        </button>
-
-        <button>
-          <Link to="/allpostings">All Postings</Link>
-        </button>
-
-        <button>
-          <Link to="/favorite">Favorite</Link>
-        </button>
-        {props.user[0]? 
-        <>
-        { props.user[0]._id ?
-        (<>
-        <button
-          onClick={() => {
-            logOutCallback();
-          }}
-        >
-          <Link to="/"> log Out</Link>
-        </button>
-        <button
-        >
-          <Link to="/post">post item</Link>
-        </button>
-        </>
+      <div
+        id="navbar"
+        className="navbar navbar-expand-lg navbar-light bg-light"
+      >
+        <div class="button">
+          <div class="bottom"></div>
+          <div class="top">
+            <Link to="/home" className="label">
+              Home
+            </Link>
+            <div class="button-border button-border-left"></div>
+            <div class="button-border button-border-top"></div>
+            <div class="button-border button-border-right"></div>
+            <div class="button-border button-border-bottom"></div>
+          </div>
+        </div>
+        <div class="button">
+          <div class="bottom"></div>
+          <div class="top">
+            <Link to="/favorite" className="label">
+              Favorite
+            </Link>
+            <div class="button-border button-border-left"></div>
+            <div class="button-border button-border-top"></div>
+            <div class="button-border button-border-right"></div>
+            <div class="button-border button-border-bottom"></div>
+          </div>
+        </div>
+        {props.user[0] ? (
+          <>
+            {props.user[0]._id ? (
+              <>
+                <div class="button">
+                  <div class="bottom"></div>
+                  <div class="top">
+                    <Link
+                      to="/"
+                      className="label"
+                      onClick={() => {
+                        logOutCallback();
+                      }}
+                    >
+                      {" "}
+                      log Out
+                    </Link>
+                    <div class="button-border button-border-left"></div>
+                    <div class="button-border button-border-top"></div>
+                    <div class="button-border button-border-right"></div>
+                    <div class="button-border button-border-bottom"></div>
+                  </div>
+                </div>
+                <div class="button">
+                  <div class="bottom"></div>
+                  <div class="top">
+                    <Link to="/post" className="label">
+                      post item
+                    </Link>
+                    <div class="button-border button-border-left"></div>
+                    <div class="button-border button-border-top"></div>
+                    <div class="button-border button-border-right"></div>
+                    <div class="button-border button-border-bottom"></div>
+                  </div>
+                </div>
+              </>
+            ) : (
+              <>
+                <div class="button">
+                  <div class="bottom"></div>
+                  <div class="top">
+                    {path == "/login" ? (
+                      ""
+                    ) : (
+                      <Link to="/" className="label">
+                        Login
+                      </Link>
+                    )}
+                    <div class="button-border button-border-left"></div>
+                    <div class="button-border button-border-top"></div>
+                    <div class="button-border button-border-right"></div>
+                    <div class="button-border button-border-bottom"></div>
+                  </div>
+                </div>
+                <div class="button">
+                  <div class="bottom"></div>
+                  <div class="top">
+                    {path == "/signUp" ? (
+                      ""
+                    ) : (
+                      <Link to="/signUp" className="label">
+                        SignUp
+                      </Link>
+                    )}
+                    <div class="button-border button-border-left"></div>
+                    <div class="button-border button-border-top"></div>
+                    <div class="button-border button-border-right"></div>
+                    <div class="button-border button-border-bottom"></div>
+                  </div>
+                </div>
+              </>
+            )}
+          </>
         ) : (
-        <>
-        {path == "/login" ? (
-          ""
-        ) : (
-            <button>
-              <Link to="/">Login</Link>
-            </button>
-          )}
-        {path == "/signUp" ? (
-          ""
-        ) : (
-
-          <button>
-            <Link to="/signUp">SignUp</Link>
-          </button>
+          <></>
         )}
-        </>
-        )
-        }</>:<></>
-        }
-
       </div>
     </div>
   );
 }
-
 export default Navbar;
