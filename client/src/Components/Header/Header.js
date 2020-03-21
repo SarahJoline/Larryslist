a9b1ac6b9a782c0d3ce2221d943fc9aa607844fc
hatout01 <hatout1@gmail.com>
2020-03-21 03:49:57 -0700

    fixed nav styling and some other styling on All posts

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -3,12 +3,12 @@ import "./header.css";
 import Navbar from "../Navbar/Navbar";
 
 function Header(props) {
-  console.log(props.user)
+  console.log(props.user);
   return (
-    <div className="jumbotron jumbotron-fluid" id="header">
+    <div className="jumbotron jumbotron-fluid" id="headerNav">
       <div className="container">
         <h1 className="display-4">LarrysList</h1>
-        <Navbar user={props.user}/>
+        <Navbar user={props.user} />
         <p className="lead"></p>
       </div>
     </div>
--- client/src/pages/AllPostings/AllPostings.js
+++ client/src/pages/AllPostings/AllPostings.js
@@ -4,81 +4,79 @@ import "./allpostings.css";
 import Axios from "axios";
 
 function AllPostings(props) {
-    const [posts, setposts] = useState({ posts: [] });
+  const [posts, setposts] = useState({ posts: [] });
 
-    useEffect(() => {
-        API.getposts().then(res => {
-            //console.log(res.data[0].comments)
-            setposts({ posts: res.data });
-        });
-    }, []);
+  useEffect(() => {
+    API.getposts().then(res => {
+      //console.log(res.data[0].comments)
+      setposts({ posts: res.data });
+    });
+  }, []);
 
-    useEffect(() => {
-        API.getposts().then(res => {
-            //console.log(res.data[0].comments)
-            setposts({ posts: res.data })
-        })
-    }, [])
+  useEffect(() => {
+    API.getposts().then(res => {
+      //console.log(res.data[0].comments)
+      setposts({ posts: res.data });
+    });
+  }, []);
 
-
-    return (
-        <div>
-            <div className="header-container">
-                <h2 className="header">All Postings:</h2>
-            </div>
-            <div className="post-container">
-                <div className="postingss">
-                    {posts.posts.map((post, i) => {
-                        return (
-                            <div className="render-post">
-                                <div className="card text-white bg-danger mb-3" key={i}>
-                                    <div className="card-header">
-                                        <h5>Title: {post.title}</h5>
-                                    </div>
-                                    <div className="card-body">
-                                        <h5 className="card-title">Category: {post.category}</h5>
-                                        <p className="card-text">Description: {post.description}</p>
-                                        <input
-                                            className="btn btn-dark"
-                                            id={post._id}
-                                            type="submit"
-                                            value="Save to Favorites"
-                                            onClick={
-                                                () => {
-                                                    //console.log(post._id)
-                                                    let id = post._id
-                                                    Axios.patch(`/api/favorite/${id}`, { saved: true })
-                                                        .then(res => console.log(res))
-                                                }
-                                            }
-                                        ></input>
-                                        <div className="form-group">
-                                            <label for="exampleFormControlTextarea1">
-                                                Add Comment: 🤬
-                                            </label>
-                                            <textarea
-                                                placeholder="work in progress.."
-                                                class="form-control"
-                                                id="exampleFormControlTextarea1"
-                                                rows="3"
-                                            ></textarea>
-                                            <input
-                                                type="submit"
-                                                value="add comment"
-                                                className="btn btn-dark"
-                                                onClick={() => {
-                                                    alert('You have commented!')
-                                                }}
-                                            />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        )
-                    })}
+  return (
+    <div>
+      <div className="header-container">
+        <h2 className="header">All Postings:</h2>
+      </div>
+      <div className="post-container">
+        <div className="postingss">
+          {posts.posts.map((post, i) => {
+            return (
+              <div className="render-post">
+                <div className="card text-white bg-danger mb-3" key={i}>
+                  <div className="card-header">
+                    <h5>Title: {post.title}</h5>
+                  </div>
+                  <div className="card-body">
+                    <h5 className="card-title">Category: {post.category}</h5>
+                    <p className="card-text">Description: {post.description}</p>
+                    <input
+                      className="btn btn-dark"
+                      id={post._id}
+                      type="submit"
+                      value="Save to Favorites"
+                      onClick={() => {
+                        //console.log(post._id)
+                        let id = post._id;
+                        Axios.patch(`/api/favorite/${id}`, {
+                          saved: true
+                        }).then(res => console.log(res));
+                      }}
+                    ></input>
+                    <div className="form-group">
+                      <label htmlFor="exampleFormControlTextarea1">
+                        Add Comment: 🤬
+                      </label>
+                      <textarea
+                        placeholder="work in progress.."
+                        className="form-control"
+                        id="exampleFormControlTextarea1"
+                        rows="3"
+                      ></textarea>
+                      <input
+                        type="submit"
+                        value="add comment"
+                        className="btn btn-dark"
+                        onClick={() => {
+                          alert("You have commented!");
+                        }}
+                      />
+                    </div>
+                  </div>
                 </div>
-            </div >
+              </div>
+            );
+          })}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 export default AllPostings;
